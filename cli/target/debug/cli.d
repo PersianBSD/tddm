22635618573f3dbb9e550c2c7b5b3ef66eec84e3
persianbsd <wxdevcpp@gmail.com>
2025-06-13 22:39:30 +0330

    ساختار اولیه پروژه PDDM ایجاد شد

---
+++ cli/target/debug/cli.d
@@ -0,0 +1,1 @@
+D:\plasma\pddm\cli\target\debug\cli.exe: D:\plasma\pddm\cli\src\main.rs
---
+++ cli/target/debug/deps/cli.d
@@ -0,0 +1,5 @@
+D:\plasma\pddm\cli\target\debug\deps\cli.exe: src\main.rs
+
+D:\plasma\pddm\cli\target\debug\deps\cli.d: src\main.rs
+
+src\main.rs:
